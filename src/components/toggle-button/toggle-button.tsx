a5d1740e9a0c483f2fc7d3d2954f763821584281
Andrzej Templin <andrzej.templin@capgemini.com>
2020-12-07 16:08:57 +0100

    initial state

--- src/components/toggle-button/toggle-button.tsx
+++ src/components/toggle-button/toggle-button.tsx
@@ -1,4 +1,4 @@
-import {Component, Prop, h, Host, Listen, Watch, Event, EventEmitter, State} from '@stencil/core';
+import { Component } from '@stencil/core';
 
 @Component({
   tag: 'toggle-button',
@@ -6,32 +6,5 @@ import {Component, Prop, h, Host, Listen, Watch, Event, EventEmitter, State} fro
   shadow: true
 })
 export class ToggleButton {
-  @Prop() checked: boolean;
-  @State() value: boolean;
-  @Event({bubbles: true, composed: true}) stateHasBeenChanged: EventEmitter;
-
-  @Listen('click')
-  changeState() {
-    this.value = !this.value;
-    this.stateHasBeenChanged.emit(this.value);
-  }
-
-  @Watch('checked')
-  onStateBeingChanged() {
-    this.checked ? this.value = true : this.value = false;
-  }
-
-    componentWillLoad() {
-      this.value = this.checked;
-    }
-
-  render() {
-    return (
-      <Host class={{'toggle-on': this.value}}>
-        <div class="switch">
-          <div class="slider round"></div>
-        </div>
-      </Host>
-    )
-  }
+  
 }
